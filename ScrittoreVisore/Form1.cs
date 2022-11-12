fba5229bdb787f510d82ef2481d83202f9bdbd48
DomeManca <domy.manca.ciao@gmail.com>
2022-11-12 14:29:29 +0100

    Aggiungere i file di progetto.

---
+++ ScrittoreVisore/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ScrittoreVisore
+{
+    public partial class Form1 : Form
+    {
+        private Visualizzatore vis;
+        private Scrittore scr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            scr = new Scrittore(vis);
+
+            richTextBox2.ReadOnly = true;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "" && comboBox1.Text != "" && colorDialog1.Color != Color.Empty)
+            {
+                scr.Nome = textBox1.Text;
+                scr.Testo = textBox2.Text;
+
+                FontStyle f;
+                Font font = richTextBox2.SelectionFont;
+                if (font != null)
+                {
+                    f = font.Style;
+                    if (comboBox1.Text == "Grassetto")
+                        f ^= FontStyle.Bold;
+                    else if (comboBox1.Text == "Corsivo")
+                        f ^= FontStyle.Italic;
+                    else if (comboBox1.Text == "Sottolineato")
+                        f ^= FontStyle.Underline;
+                    else if (comboBox1.Text == "Default")
+                        f ^= FontStyle.Regular;
+
+                    richTextBox2.SelectionFont = new Font(font, f);
+
+                    richTextBox2.SelectionColor = colorDialog1.Color;
+
+                    richTextBox2.AppendText(scr.Nome + ": " + scr.Testo + "\n");
+                }
+
+                textBox1.Text = "";
+                textBox2.Text = "";
+                comboBox1.Text = "";
+                colorDialog1.Color = Color.Empty;
+            }
+            else
+                MessageBox.Show("Inserire tutti i Valori");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+        }
+    }
+}
---
+++ ScrittoreVisore/Visualizzatore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrittoreVisore
+{
+    internal class Visualizzatore
+    {
+        //attributi
+        private string testo;
+
+        //costruttori
+
+        //metodi
+        public string Testo
+        {
+            set
+            {
+                testo = value;
+            }
+
+            get
+            {
+                return testo;
+            }
+        }
+        public void AggiungiTesto(string testo)
+        {
+            Testo += testo;
+        }
+    }
+}
